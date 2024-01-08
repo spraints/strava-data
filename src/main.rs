eb60ff3628a65f0049dcd64214d9245eeebc2973
Matt Burke <spraints@gmail.com>
2024-01-07 21:10:55 -0500

    bad parsing oh noetry

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,12 @@ fn parse_activities_csv<P: AsRef<Path>>(path: P) -> anyhow::Result<Vec<ActivityS
     let mut rdr = csv::Reader::from_path(path)?;
     let mut res = Vec::new();
     for record in rdr.records().skip(1) {
-        res.push(record?.try_into()?);
+        let record = record?;
+        res.push(
+            (&record)
+                .try_into()
+                .or_else(|e| Err(anyhow::anyhow!("{e} while parsing {}", record.as_slice())))?,
+        );
     }
     Ok(res)
 }
@@ -49,12 +54,18 @@ struct Archive {
 struct ActivitySummary {
     id: u128,
     date: chrono::DateTime<Utc>,
+    // todo: figure out which of these fields I'm interested in:
+    //   Activity ID,Activity Date,Activity Name,Activity Type,Activity Description,Elapsed Time,Distance,Max Heart Rate,Relative Effort,Commute,Activity Private Note,Activity Gear,Filename,Athlete Weight,Bike Weight,Elapsed Time,Moving Time,Distance,Max Speed,Average Speed,Elevation Gain,Elevation Loss,Elevation Low,Elevation High,Max Grade,Average Grade,Average Positive Grade,Average Negative Grade,Max Cadence,Average Cadence,Max Heart Rate,Average Heart Rate,Max Watts,Average Watts,Calories,Max Temperature,Average Temperature,Relative Effort,Total Work,Number of Runs,Uphill Time,Downhill Time,Other Time,Perceived Exertion,Type,Start Time,Weighted Average Power,Power Count,Prefer Perceived Exertion,Perceived Relative Effort,Commute,Total Weight Lifted,From Upload,Grade Adjusted Distance,Weather Observation Time,Weather Condition,Weather Temperature,Apparent Temperature,Dewpoint,Humidity,Weather Pressure,Wind Speed,Wind Gust,Wind Bearing,Precipitation Intensity,Sunrise Time,Sunset Time,Moon Phase,Bike,Gear,Precipitation Probability,Precipitation Type,Cloud Cover,Weather Visibility,UV Index,Weather Ozone,Jump Count,Total Grit,Average Flow,Flagged,Average Elapsed Speed,Dirt Distance,Newly Explored Distance,Newly Explored Dirt Distance,Activity Count,Total Steps,Media
+    // - My initial point of curiosity was date,activity_type,max_heart_rate so that I could see how
+    //   heart rate varied per activity. If avg heart rate is available, that would be nice to see,
+    //   too.
+    // - I think I'd also be interested in plotting distance and time for run and/or bike.
 }
 
-impl TryFrom<StringRecord> for ActivitySummary {
+impl TryFrom<&StringRecord> for ActivitySummary {
     type Error = anyhow::Error;
 
-    fn try_from(value: StringRecord) -> Result<Self, Self::Error> {
+    fn try_from(value: &StringRecord) -> Result<Self, Self::Error> {
         fn f(v: &StringRecord, i: usize) -> anyhow::Result<&str> {
             v.get(i).ok_or_else(|| {
                 anyhow::anyhow!("tried to get field {i} from row with {} fields", v.len())
@@ -62,7 +73,10 @@ impl TryFrom<StringRecord> for ActivitySummary {
         }
         Ok(Self {
             id: f(&value, 0)?.parse()?,
-            date: NaiveDateTime::parse_from_str(f(&value, 1)?, "%b %d, %Y, %H:%M:%S %p")?.and_utc(),
+            // todo: chrono seems to require either space-padded or 0-padded day number and hour.
+            // >:(
+            date: NaiveDateTime::parse_from_str(f(&value, 1)?, "%b %-d, %Y, %-H:%M:%S %p")?
+                .and_utc(),
         })
     }
 }
