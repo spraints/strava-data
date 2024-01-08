7abf729c0dca5d9ea5999e1532093c94e7abe5a5
Matt Burke <spraints@gmail.com>
2024-01-08 10:49:19 -0500

    more fields

--- src/main.rs
+++ src/main.rs
@@ -54,6 +54,16 @@ struct Archive {
 struct ActivitySummary {
     id: u128,
     date: DateTime<Utc>,
+    name: String,
+    act_type: ActivityType,
+    elapsed_seconds: u32,
+    distance: f32,
+    max_heart_rate: Option<f32>,
+    filename: String,
+    elapsed_seconds2: Option<f32>,
+    moving_seconds: Option<f32>,
+    distance2: f32,
+    max_speed: Option<f32>,
     // todo: figure out which of these fields I'm interested in:
     //   Activity ID,Activity Date,Activity Name,Activity Type,Activity Description,Elapsed Time,Distance,Max Heart Rate,Relative Effort,Commute,Activity Private Note,Activity Gear,Filename,Athlete Weight,Bike Weight,Elapsed Time,Moving Time,Distance,Max Speed,Average Speed,Elevation Gain,Elevation Loss,Elevation Low,Elevation High,Max Grade,Average Grade,Average Positive Grade,Average Negative Grade,Max Cadence,Average Cadence,Max Heart Rate,Average Heart Rate,Max Watts,Average Watts,Calories,Max Temperature,Average Temperature,Relative Effort,Total Work,Number of Runs,Uphill Time,Downhill Time,Other Time,Perceived Exertion,Type,Start Time,Weighted Average Power,Power Count,Prefer Perceived Exertion,Perceived Relative Effort,Commute,Total Weight Lifted,From Upload,Grade Adjusted Distance,Weather Observation Time,Weather Condition,Weather Temperature,Apparent Temperature,Dewpoint,Humidity,Weather Pressure,Wind Speed,Wind Gust,Wind Bearing,Precipitation Intensity,Sunrise Time,Sunset Time,Moon Phase,Bike,Gear,Precipitation Probability,Precipitation Type,Cloud Cover,Weather Visibility,UV Index,Weather Ozone,Jump Count,Total Grit,Average Flow,Flagged,Average Elapsed Speed,Dirt Distance,Newly Explored Distance,Newly Explored Dirt Distance,Activity Count,Total Steps,Media
     // - My initial point of curiosity was date,activity_type,max_heart_rate so that I could see how
@@ -75,9 +85,67 @@ impl TryFrom<&StringRecord> for ActivitySummary {
             })
         }
         Ok(Self {
-            id: f(&value, 0, "id")?.parse()?,
-            date: parse_date(f(&value, 1, "date")?)?,
+            // todo - figure out which of these fields are worth keeping.
+            // todo - use serde?
+            id: f(&value, 0, "Activity ID")?.parse()?,
+            date: parse_date(f(&value, 1, "Activity Date")?)?,
+            name: f(&value, 2, "Activity Name")?.to_string(),
+            act_type: f(&value, 3, "Activity Type")?.try_into()?,
+            elapsed_seconds: f(&value, 5, "Elapsed Time")?.parse()?,
+            distance: f(&value, 6, "Distance")?.parse()?,
+            max_heart_rate: match f(&value, 7, "Max Heart Rate")? {
+                "" => None,
+                s => Some(s.parse()?),
+            },
+            filename: f(&value, 12, "Filename")?.to_string(),
+            elapsed_seconds2: match f(&value, 15, "Elapsed Time")? {
+                "" => None,
+                s => Some(s.parse()?),
+            },
+            moving_seconds: match f(&value, 16, "Moving Time")? {
+                "" => None,
+                s => Some(s.parse()?),
+            },
+            distance2: f(&value, 17, "Distance")?.parse()?,
+            max_speed: match f(&value, 18, "Max Speed")? {
+                "" => None,
+                s => Some(s.parse()?),
+            },
+            // todo - parse some more
         })
+    }
+}
+
+enum ActivityType {
+    AlpineSki,
+    Hike,
+    IceSkate,
+    Ride,
+    Run,
+    Walk,
+    WeightTraining,
+    Workout,
+    Yoga,
+}
+
+impl TryFrom<&str> for ActivityType {
+    type Error = anyhow::Error;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        // todo - use serde for this??
+        let res = match value {
+            "Alpine Ski" => ActivityType::AlpineSki,
+            "Hike" => ActivityType::Hike,
+            "Ice Skate" => ActivityType::IceSkate,
+            "Ride" => ActivityType::Ride,
+            "Run" => ActivityType::Run,
+            "Walk" => ActivityType::Walk,
+            "Weight Training" => ActivityType::WeightTraining,
+            "Workout" => ActivityType::Workout,
+            "Yoga" => ActivityType::Yoga,
+            _ => anyhow::bail!("unrecognized activity type {value:?}"),
+        };
+        Ok(res)
     }
 }
 
