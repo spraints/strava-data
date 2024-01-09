82a7e0e0ff1a8198d0ba9065a03294a89b771869
Matt Burke <spraints@gmail.com>
2024-01-09 08:28:49 -0500

    add a couple more fields

--- src/main.rs
+++ src/main.rs
@@ -64,8 +64,9 @@ struct ActivitySummary {
     moving_seconds: Option<f32>,
     distance2: f32,
     max_speed: Option<f32>,
-    // todo: figure out which of these fields I'm interested in:
-    //   Activity ID,Activity Date,Activity Name,Activity Type,Activity Description,Elapsed Time,Distance,Max Heart Rate,Relative Effort,Commute,Activity Private Note,Activity Gear,Filename,Athlete Weight,Bike Weight,Elapsed Time,Moving Time,Distance,Max Speed,Average Speed,Elevation Gain,Elevation Loss,Elevation Low,Elevation High,Max Grade,Average Grade,Average Positive Grade,Average Negative Grade,Max Cadence,Average Cadence,Max Heart Rate,Average Heart Rate,Max Watts,Average Watts,Calories,Max Temperature,Average Temperature,Relative Effort,Total Work,Number of Runs,Uphill Time,Downhill Time,Other Time,Perceived Exertion,Type,Start Time,Weighted Average Power,Power Count,Prefer Perceived Exertion,Perceived Relative Effort,Commute,Total Weight Lifted,From Upload,Grade Adjusted Distance,Weather Observation Time,Weather Condition,Weather Temperature,Apparent Temperature,Dewpoint,Humidity,Weather Pressure,Wind Speed,Wind Gust,Wind Bearing,Precipitation Intensity,Sunrise Time,Sunset Time,Moon Phase,Bike,Gear,Precipitation Probability,Precipitation Type,Cloud Cover,Weather Visibility,UV Index,Weather Ozone,Jump Count,Total Grit,Average Flow,Flagged,Average Elapsed Speed,Dirt Distance,Newly Explored Distance,Newly Explored Dirt Distance,Activity Count,Total Steps,Media
+    avg_speed: Option<f32>,
+    elevation_high: Option<f32>,
+    elevation_low: Option<f32>,
     // - My initial point of curiosity was date,activity_type,max_heart_rate so that I could see how
     //   heart rate varied per activity. If avg heart rate is available, that would be nice to see,
     //   too.
@@ -111,7 +112,18 @@ impl TryFrom<&StringRecord> for ActivitySummary {
                 "" => None,
                 s => Some(s.parse()?),
             },
-            // todo - parse some more
+            avg_speed: match f(&value, 19, "Average Speed")? {
+                "" => None,
+                s => Some(s.parse()?),
+            },
+            elevation_low: match f(&value, 22, "Elevation Low")? {
+                "" => None,
+                s => Some(s.parse()?),
+            },
+            elevation_high: match f(&value, 23, "Elevation High")? {
+                "" => None,
+                s => Some(s.parse()?),
+            },
         })
     }
 }
