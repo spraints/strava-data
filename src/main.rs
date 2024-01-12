8b5de384dc0b4bb1c7d53a506abfb4d6b868c3e6
Matt Burke <spraints@gmail.com>
2024-01-11 19:53:23 -0500

    Back to serde, with a fix for duplicate columns

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ mod args;
 use args::{Cli, Command};
 use chrono::{DateTime, NaiveDateTime, Utc};
 use clap::Parser;
-use csv::StringRecord;
+use serde::{Deserialize, Deserializer};
+use std::borrow::Cow;
 use std::fs::DirEntry;
 use std::path::Path;
 
@@ -34,16 +35,31 @@ fn parse_dir_entry(archive: &mut Archive, e: DirEntry) -> anyhow::Result<()> {
 
 fn parse_activities_csv<P: AsRef<Path>>(path: P) -> anyhow::Result<Vec<ActivitySummary>> {
     let mut rdr = csv::Reader::from_path(path)?;
+    let fixed_headers = fix_headers(rdr.headers()?);
+    rdr.set_headers(fixed_headers);
     let mut res = Vec::new();
-    for record in rdr.records().skip(1) {
-        let record = record?;
-        res.push(
-            (&record)
-                .try_into()
-                .or_else(|e| Err(anyhow::anyhow!("{e} while parsing {}", record.as_slice())))?,
-        );
+    for record in rdr.deserialize() {
+        res.push(record?);
     }
     Ok(res)
+}
+
+fn fix_headers(header: &csv::StringRecord) -> csv::StringRecord {
+    let mut headers: Vec<Cow<str>> = Vec::new();
+    for field in header {
+        if headers.contains(&Cow::Borrowed(field)) {
+            for suffix in 2.. {
+                let new_field = Cow::Owned(format!("{field} ({suffix})"));
+                if !headers.contains(&new_field) {
+                    headers.push(new_field);
+                    break;
+                }
+            }
+        } else {
+            headers.push(Cow::Borrowed(field));
+        }
+    }
+    headers.into()
 }
 
 #[derive(Default)]
@@ -51,126 +67,70 @@ struct Archive {
     activities: Vec<ActivitySummary>,
 }
 
+#[derive(Deserialize)]
 struct ActivitySummary {
+    #[serde(rename = "Activity ID")]
     id: u128,
+    #[serde(rename = "Activity Date", deserialize_with = "deserialize_date")]
     date: DateTime<Utc>,
+    #[serde(rename = "Activity Name")]
     name: String,
+    #[serde(rename = "Activity Type")]
     act_type: ActivityType,
+    #[serde(rename = "Elapsed Time")]
     elapsed_seconds: u32,
+    #[serde(rename = "Distance")]
     distance: f32,
+    #[serde(rename = "Max Heart Rate")]
     max_heart_rate: Option<f32>,
+    #[serde(rename = "Filename")]
     filename: String,
+    #[serde(rename = "Elapsed Time (2)")]
     elapsed_seconds2: Option<f32>,
+    #[serde(rename = "Moving Time")]
     moving_seconds: Option<f32>,
+    #[serde(rename = "Distance (2)")]
     distance2: f32,
+    #[serde(rename = "Max Speed")]
     max_speed: Option<f32>,
+    #[serde(rename = "Average Speed")]
     avg_speed: Option<f32>,
-    elevation_high: Option<f32>,
+    #[serde(rename = "Elevation Low")]
     elevation_low: Option<f32>,
+    #[serde(rename = "Elevation High")]
+    elevation_high: Option<f32>,
     // - My initial point of curiosity was date,activity_type,max_heart_rate so that I could see how
     //   heart rate varied per activity. If avg heart rate is available, that would be nice to see,
     //   too.
     // - I think I'd also be interested in plotting distance and time for run and/or bike.
 }
 
-impl TryFrom<&StringRecord> for ActivitySummary {
-    type Error = anyhow::Error;
-
-    fn try_from(value: &StringRecord) -> Result<Self, Self::Error> {
-        fn f<'b>(v: &'b StringRecord, i: usize, label: &str) -> anyhow::Result<&'b str> {
-            v.get(i).ok_or_else(|| {
-                anyhow::anyhow!(
-                    "tried to get {label} field {i} from row with {} fields",
-                    v.len()
-                )
-            })
-        }
-        Ok(Self {
-            // todo - figure out which of these fields are worth keeping.
-            // todo - use serde?
-            id: f(&value, 0, "Activity ID")?.parse()?,
-            date: parse_date(f(&value, 1, "Activity Date")?)?,
-            name: f(&value, 2, "Activity Name")?.to_string(),
-            act_type: f(&value, 3, "Activity Type")?.try_into()?,
-            elapsed_seconds: f(&value, 5, "Elapsed Time")?.parse()?,
-            distance: f(&value, 6, "Distance")?.parse()?,
-            max_heart_rate: match f(&value, 7, "Max Heart Rate")? {
-                "" => None,
-                s => Some(s.parse()?),
-            },
-            filename: f(&value, 12, "Filename")?.to_string(),
-            elapsed_seconds2: match f(&value, 15, "Elapsed Time")? {
-                "" => None,
-                s => Some(s.parse()?),
-            },
-            moving_seconds: match f(&value, 16, "Moving Time")? {
-                "" => None,
-                s => Some(s.parse()?),
-            },
-            distance2: f(&value, 17, "Distance")?.parse()?,
-            max_speed: match f(&value, 18, "Max Speed")? {
-                "" => None,
-                s => Some(s.parse()?),
-            },
-            avg_speed: match f(&value, 19, "Average Speed")? {
-                "" => None,
-                s => Some(s.parse()?),
-            },
-            elevation_low: match f(&value, 22, "Elevation Low")? {
-                "" => None,
-                s => Some(s.parse()?),
-            },
-            elevation_high: match f(&value, 23, "Elevation High")? {
-                "" => None,
-                s => Some(s.parse()?),
-            },
-        })
-    }
-}
-
+#[derive(Deserialize)]
 enum ActivityType {
+    #[serde(rename = "Alpine Ski")]
     AlpineSki,
     Hike,
+    #[serde(rename = "Ice Skate")]
     IceSkate,
     Ride,
     Run,
     Walk,
+    #[serde(rename = "Weight Training")]
     WeightTraining,
     Workout,
     Yoga,
 }
 
-impl TryFrom<&str> for ActivityType {
-    type Error = anyhow::Error;
-
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        // todo - use serde for this??
-        let res = match value {
-            "Alpine Ski" => ActivityType::AlpineSki,
-            "Hike" => ActivityType::Hike,
-            "Ice Skate" => ActivityType::IceSkate,
-            "Ride" => ActivityType::Ride,
-            "Run" => ActivityType::Run,
-            "Walk" => ActivityType::Walk,
-            "Weight Training" => ActivityType::WeightTraining,
-            "Workout" => ActivityType::Workout,
-            "Yoga" => ActivityType::Yoga,
-            _ => anyhow::bail!("unrecognized activity type {value:?}"),
-        };
-        Ok(res)
-    }
+fn deserialize_date<'de, D>(d: D) -> Result<DateTime<Utc>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    String::deserialize(d)
+        .and_then(|s| parse_date(&s).map_err(|e| serde::de::Error::custom(e.to_string())))
 }
 
-fn parse_date(date_str: &str) -> anyhow::Result<DateTime<Utc>> {
-    Ok(
-        NaiveDateTime::parse_from_str(date_str, "%b %-d, %Y, %-I:%M:%S %p")
-            .or_else(|e| {
-                Err(anyhow::anyhow!(
-                    "could not parse date string {date_str:?}: {e}"
-                ))
-            })?
-            .and_utc(),
-    )
+fn parse_date(s: &str) -> Result<DateTime<Utc>, chrono::ParseError> {
+    NaiveDateTime::parse_from_str(&s, "%b %-d, %Y, %-I:%M:%S %p").map(|dt| dt.and_utc())
 }
 
 #[cfg(test)]
